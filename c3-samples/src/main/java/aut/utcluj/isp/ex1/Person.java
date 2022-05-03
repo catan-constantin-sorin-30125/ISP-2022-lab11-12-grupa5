4d2aa2d75a4baad4a0306474210f03775bcc196c
930r <59374337+930r@users.noreply.github.com>
2022-05-03 17:41:24 +0300

    actually finished ex1

--- c3-samples/src/main/java/aut/utcluj/isp/ex1/Person.java
+++ c3-samples/src/main/java/aut/utcluj/isp/ex1/Person.java
@@ -7,13 +7,13 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    public Person(String lastName) {
-        this.lastName = lastName;
+    public Person(String firstName) {
+        this.lastName = "";
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public Person(String firstName, String lastName) {
-        this.lastName = lastName;
-        this.firstName = firstName;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     public String getFirstName() {
@@ -22,5 +22,24 @@ public class Person {
 
     public String getLastName() {
         return lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        //Check for null and compare run-time types.
+        final Person other = (Person) obj;
+        if ((this.firstName==other.firstName)&&(this.lastName==other.lastName))
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  firstName +" "+ lastName ;
     }
 }
