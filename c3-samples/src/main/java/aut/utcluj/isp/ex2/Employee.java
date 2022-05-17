64c6fc4b773ca2efb902e50d419205be66afae9c
930r <59374337+930r@users.noreply.github.com>
2022-05-17 05:04:02 +0300

    ex2 employee

--- c3-samples/src/main/java/aut/utcluj/isp/ex2/Employee.java
+++ c3-samples/src/main/java/aut/utcluj/isp/ex2/Employee.java
@@ -3,11 +3,11 @@ package aut.utcluj.isp.ex2;
 /**
  * @author stefan
  */
-public class Employee {
+public class Employee extends Person{
     private Double salary;
-
     public Employee(String firstName, String lastName, Double salary) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        super(firstName,lastName);
+        this.salary = salary;
     }
 
     public Double getSalary() {
@@ -19,6 +19,6 @@ public class Employee {
      * @return employee information (Firstname: firstname Lastname: lastname Salary: salary)
      */
     public String showEmployeeInfo() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return "Firstname: "+super.firstName+" Lastname: "+super.lastName+" Salary: "+this.salary;
     }
 }
